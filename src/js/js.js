7b422fc435474b0d0f926c330185bbe9796b115d
Nazar <1870943144@qq.com>
2022-06-21 11:33:51 +0300

    Home work was done (start created new app)

--- src/js/js.js
+++ src/js/js.js
@@ -1,6 +1,19 @@
-const obj = {
-    a: 1,
-    '2': 3,
-    3: 10
+const numberOfFilms = prompt('how many films did you watched?', '');
+ 
+// second
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: {},
+    privat: false
 };
-console.log(obj.[a]);
+
+// third
+
+const lastFilm = prompt('Your last film?', ''),
+      levelFilm = prompt('Films level');
+personalMovieDB.movies[lastFilm] = levelFilm;
+
+console.log(personalMovieDB);
